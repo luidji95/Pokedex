420a231906dc26b53bf5d4d2fbdd198d5977cf43
Milos Petrovic <milos.petrovic09.95@gmail.com>
2024-09-16 23:28:30 +0200

    creating manager class

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
 import './style.css'
+
+
+'use strict';
+
+const container = document.querySelector('.container'); 
+const pokedexList = document.querySelector('.pokedex-list');
+const findPokeInput = document.querySelector('.findPoke');
+
+class PokemonManager {
+    constructor() {
+        this.pokemonsArray = [];
+        this.singlePokemon = "";
+    }
+
+    addPokemon(name, index) {
+        this.pokemonsArray.push({ name, index: index + 1 });
+    }
+
+    renderPokemons() {
+        pokedexList.innerHTML = ''; 
+
+        this.pokemonsArray.forEach(pokemon => {
+            let listItem = document.createElement('li');
+            listItem.textContent = `${pokemon.index}. ${pokemon.name}`;
+            pokedexList.appendChild(listItem);
+        });
+    }
+
+    resetPokemons() {
+        this.pokemonsArray = [];
+    }
+
+ 
+}
